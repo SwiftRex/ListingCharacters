c22a983fe40823d8e8e593cefa7248af3a129cab
Jarbas B <bmsjarbas@gmail.com>
2020-12-05 09:21:07 -0300

    Initial Commit

---
+++ ListingCharacters/ListingCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListingCharactersApp.swift
+//  ListingCharacters
+//
+//  Created by Jarbas on 05/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ListingCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
