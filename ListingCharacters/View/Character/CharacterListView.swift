497de45270a42364b79ee0de9eeb0b9f768da43b
Jarbas B <bmsjarbas@gmail.com>
2020-12-06 17:12:31 -0300

    create dummy viewmodel and first draft of character list view

--- ListingCharacters/View/Character/CharacterListView.swift
+++ ListingCharacters/View/Character/CharacterListView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct CharacterListView: View {
+    @ObservedObject var viewModel =  CharacterListViewModel()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(self.viewModel.characterList) { character in
+                CharacterRow(character: character)
+            }
+            .navigationTitle("Character List")
+        }.onAppear {
+            self.viewModel.getCharacterList()
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         CharacterListView()
---
+++ ListingCharacters/ViewModel/CharacterListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterListViewModel.swift
+//  ListingCharacters
+//
+//  Created by Jarbas on 06/12/20.
+//
+
+import Combine
+import SwiftUI
+
+class CharacterListViewModel: ObservableObject {
+    var characterList = [Character]()
+
+    func getCharacterList() {        
+    }
+}
